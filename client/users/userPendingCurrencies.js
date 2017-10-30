17bd2a5f366b969fbfbff2a49b8888307aaaf037
gazhayes <gareth.hayes@gmail.com>
2017-10-30 21:38:50 +0800

    Problem: users cannot see rejected currencies
    Solution: add rejected currencies to pending page

--- client/users/userPendingCurrencies.js
+++ client/users/userPendingCurrencies.js
@@ -1,8 +1,10 @@
 import { Template } from 'meteor/templating';
 import { PendingCurrencies } from '../../lib/database/Currencies.js';
+import { RejectedCurrencies } from '../../lib/database/Currencies.js';
 
 Template.userPendingCurrencies.onCreated(function bodyOnCreated() {
   Meteor.subscribe('pendingcurrencies');
+  Meteor.subscribe('rejectedcurrencies');
 });
 
 Template.userPendingCurrencies.onRendered( function () {
@@ -10,7 +12,20 @@ Template.userPendingCurrencies.onRendered( function () {
 });
 
 Template.userPendingCurrencies.helpers({
-  currencies() {
-        return PendingCurrencies.find({owner: Meteor.user()._id});
-      }
+  pendingcurrencies() {
+    if(_.size(PendingCurrencies.find({owner: Meteor.user()._id}).fetch()) == 0) {
+      Session.set('pending', "You have no currencies or changes waiting approval");
+    } else {
+      Session.set('pending', "");
+      return PendingCurrencies.find({owner: Meteor.user()._id});
+    }
+  },
+  rejectedcurrencies() {
+    if(_.size(RejectedCurrencies.find({owner: Meteor.user()._id}).fetch()) == 0) {
+      Session.set('rejectedcoins', "You have no rejected currencies or changes");
+    } else {
+      Session.set('rejectedcoins', "");
+      return RejectedCurrencies.find({owner: Meteor.user()._id});
+    }
+  },
 });
