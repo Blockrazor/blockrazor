a939a0dee839377fb6caccc30269f6cbd643d50c
gazhayes <gareth.hayes@gmail.com>
2017-11-06 10:43:15 +0800

    Problem: no form for fetching API call
    Solution: create API bounty page with form where user can add an API call and select the correct data response object

--- client/normal/bounties/bounties.js
+++ client/normal/bounties/bounties.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
-const REWARDCOEFFICIENT = 100000000;
+import './rainbow.js';
+const REWARDCOEFFICIENT = 60000000;
 
 Template.bounties.onRendered(function(){
   Session.set('workingBounty', false);
@@ -72,17 +73,68 @@ Template.bountyRender.helpers({
 });
 
 Template.bountyRender.events({
+  'click .start': function () {
+    Session.set('workingBounty', true);
+    //Session.set('bountyItem', this._id);
+    Cookies.set('bountyItem', this._id, { expires: 1});
+    Cookies.set('bountyType', this.bountyType, { expires: 1});
+    Session.set('bountyType', this.bountyType);
+    //Meteor.call('startBounty', this._id); //Make a meteor call to change the status of the bounty to being worked on by <username> and add "working on bounty" to their profile so they cant take another one at the same time.
+    FlowRouter.go("/bounties/" + this._id);
+  },
+  'click .cancel': function() {
+    $('#' + this._id).hide();
+    $('#takeBounty' + this._id).show();
+  },
   'click .takeBounty': function() {
     $('#' + this._id).show();
     $('#takeBounty' + this._id).hide();
-    //Session.set('workingBounty', true);
-    Session.set('bountyItem', this._id);
   }
 })
 
-Template.activeBounty.helpers({});
-Template.activeBounty.events({
-  'click .cancel': function() {
-    Session.set('workingBounty', false);
+Template.activeBounty.onRendered(function(){
+  Session.set('bountyType', Cookies.get('bountyType'));
+
+});
+
+Template.activeBounty.helpers({
+  thisbounty () {
+    return FlowRouter.getParam("_id");
+  },
+  thisitem: function () {
+//    console.log(this.data._id);
+    return "sdfgsdfg";
+  }
+});
+Template.activeBounty.events({});
+
+Template.APIbounty.events({
+  'click .fetch': function() {
+    var d = $('#apiCall').val();
+    var parser = $('#hashrate').val();
+    var decimal = Math.pow(10, parseInt($('#decimal').val()));
+    //console.log(parser);
+    fetch("https://cors-anywhere.herokuapp.com/" + d).then(function(result) {
+  return result.json();
+}).then(function(data) {
+  //$('#result').html("");
+  $('#hashrate-result').text(Math.round(eval($('#hashrate').val())));
+  $('#height-result').text(eval($('#height').val()));
+  $('#timestamp-result').text(eval($('#timestamp').val()));
+  $('#reward-result').text(eval($('#reward').val()) / decimal);
+  $('#emission-result').text(Math.round(eval($('#emission').val()) / decimal));
+  $('#result').html(JSON.stringify(data));
+  $('#result').rainbowJSON();
+  var emission = eval($('#emission').val());
+
+  console.log(decimal);
+  var emission = emission / decimal;
+
+  console.log("----processing result----");
+  console.log("This will return the result of the API query and parse it through the filter you have provided ( " + parser + " ) WITHOUT Math.round(), this is so that you can see the entire JSON object for diagnostics.");
+  console.log(eval(parser));
+  console.log("----done----");
+  console.log(emission);
+});
   }
 });
