da7fce24e56c9042fb8b70f1e617c052381ced9c
gazhayes <gareth.hayes@gmail.com>
2017-11-07 12:03:35 +0800

    Problem: bounties using dummy data not db
    Solution: add bounties DB, convert everything over to use database instead of an array

--- client/normal/bounties/bounties.js
+++ client/normal/bounties/bounties.js
@@ -1,6 +1,13 @@
 import { Template } from 'meteor/templating';
 import './rainbow.js';
+import { Bounties, BountyTypes } from '../../../lib/database/Bounties.js';
 const REWARDCOEFFICIENT = 60000000;
+
+Template.bounties.onCreated(function(){
+  this.autorun(() => {
+    this.subscribe('bounties');
+  });
+});
 
 Template.bounties.onRendered(function(){
   Session.set('workingBounty', false);
@@ -11,42 +18,23 @@ Template.bounties.onRendered(function(){
 
 
 Template.bounties.helpers({
-  activebounty: function () {
-    return [{
-      _id: "23562356",
-      currencyName: "Bitcoin",
-      bountyType: "API",
-      creationTime: 1509781011000,
-      currentlyAvailable: true,
-      currentUsername: null
-    }];
-  },
   bounties: function() {
-    return [{
-      _id: "23562356",
-      currencyName: "Bitcoin",
-      bountyType: "API",
-      creationTime: 1509781011000,
-      currentlyAvailable: true,
-      currentUsername: null
-    },
-    {
-      _id: "43564546",
-      currencyName: "Dash",
-      bountyType: "API",
-      creationTime: 1509776537000,
-      currentlyAvailable: false,
-      currentUsername: "Roger Ver"
-    }];
+    return Bounties.find();
   }
 });
 
+Template.bountyRender.onCreated(function(){
+  this.autorun(() => {
+    this.subscribe('bountytypes');
+  });
+});
+
 Template.bountyRender.helpers({
+  types: function () {
+    return BountyTypes.findOne();
+  },
   id: function() {
     return this._id;
-  },
-  bountyDetails: function () {
-    return "After clicking start, you will have 30 minutes to complete this bounty. You need to: [1] find a block explorer or open node which can provide the current network hash rate, [2] construct an API call to get the current hash rate, and [3] tell Blockrazor how to consume the response so that the final result is an integer."//DB call to get bounty details for this bounty type
   },
   workingText: function () {
     if(this.currentlyAvailable == false) {
@@ -57,15 +45,6 @@ Template.bountyRender.helpers({
     if(this.currentlyAvailable == true) {
         return "btn-outline-primary takeBounty";
     } else { return "btn-outline-secondary";}
-  },
-  bountyTypeHeading: function () {
-    return "Find and provide API call details"; //find the description for this.bountyType bounty type in DB
-  },
-  problem: function () {
-    return "Blockrazor doesn't know how to get the current hash power for"; //find problem description for this.bountyType from DB
-  },
-  solution: function () {
-    return "find a block explorer or open node and provide the API call (and the response object details) so that Blockrazor can keep itself updated"; //as above
   },
   reward: function () {
     return ((Session.get('now') - this.creationTime) / REWARDCOEFFICIENT).toFixed(6);
@@ -79,7 +58,7 @@ Template.bountyRender.events({
     Cookies.set('bountyItem', this._id, { expires: 1});
     Cookies.set('bountyType', this.bountyType, { expires: 1});
     Session.set('bountyType', this.bountyType);
-    //Meteor.call('startBounty', this._id); //Make a meteor call to change the status of the bounty to being worked on by <username> and add "working on bounty" to their profile so they cant take another one at the same time.
+    Meteor.call('startBounty', this._id); //Make a meteor call to change the status of the bounty to being worked on by <username> and add "working on bounty" to their profile so they cant take another one at the same time.
     FlowRouter.go("/bounties/" + this._id);
   },
   'click .cancel': function() {
@@ -94,29 +73,44 @@ Template.bountyRender.events({
 
 Template.activeBounty.onRendered(function(){
   Session.set('bountyType', Cookies.get('bountyType'));
+  Meteor.setInterval(function() {
+      Session.set('now', Date.now());
+  }, 1000);
+});
 
+Template.activeBounty.onCreated(function(){
+  this.autorun(() => {
+    this.subscribe('bounties', FlowRouter.getParam("_id"));
+  });
 });
 
 Template.activeBounty.helpers({
-  thisbounty () {
-    return FlowRouter.getParam("_id");
-  },
-  thisitem: function () {
-//    console.log(this.data._id);
-    return "sdfgsdfg";
+  timeRemaining () {
+    var minutes = Math.round((Bounties.findOne().expiresAt - Session.get('now'))/1000/60);
+    var fulltext = "Time remaining: " + minutes + " minutes.";
+    if (minutes > 0) {
+      return fulltext;
+    } else if (minutes <= 0) {
+      FlowRouter.go("/bounties");
+    }
+
   }
+  // thisbounty () {
+  //   return FlowRouter.getParam("_id");
+  // }
 });
 Template.activeBounty.events({});
 
-Template.APIbounty.helpers({
-  bountyData () {
-    return {
-      name: "Bitcoin"
-    }; //make this a database call
+Template.HashrateAPI.helpers({
+  expires () {
+    return moment(Bounties.findOne().expiresAt).fromNow();
+  },
+  thisbounty () {
+    return Bounties.findOne();
   }
 });
 
-Template.APIbounty.events({
+Template.HashrateAPI.events({
   'click .submit': function () {
     //Functions to check data:
     //Hashrate
@@ -174,7 +168,7 @@ Template.APIbounty.events({
           FlowRouter.go("/mypendingbounties");
         }
 
-      })  
+      })
     };
 
 
--- server/main.js
+++ server/main.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+  import {generateBounties, createtypes} from '../lib/database/Bounties.js';
 //import * as jobs from './API_requests/github.js';
 
 Accounts.onLogin(function(user){
+
+  //generateBounties();
+  //createtypes();
   if (user.type != "resume") {
     Meteor.call('initializeUser');
     Meteor.call('initializeActivityLog');
