5544e288f1b85008074d51eff5b7feb89ac39d3c
gazhayes <gareth.hayes@gmail.com>
2017-11-13 16:49:14 +0800

    Problem: features not showing total number of comments
    Soltion: show the total number of comments in reply to each feature

--- client/normal/currencyDetail/fundamentalMetrics.js
+++ client/normal/currencyDetail/fundamentalMetrics.js
@@ -122,6 +122,9 @@ Template.feature.onRendered(function(){
 
 })
 Template.feature.helpers({
+  numComments: function() {
+    return _.size(Features.find({parentId: this._id}).fetch());
+  },
   starsid: function() {
     return "star-" + this._id
   },
@@ -178,22 +181,24 @@ Template.feature.events({
       sAlert.error("You must be logged in to comment!");
     }
     var data = $('#replyText-' + this._id).val();
+    var ifnoterror = function(){
+    }
     if(data.length < 6 || data.length > 140) {
       sAlert.error("That entry is too short, or too long.");
     } else {
       Meteor.call('newComment', this._id, data, 1, function(error, result) {
         if(!error) {
-          $('#replyText-' + this._id).val(" ");
-          $(".newcomment-" + this._id).hide();
-          Cookies.set("submitted" + this._id, true);
-          $(".commentParent-" + this._id).hide();
-          Session.set("showingComments" + this._id, "false")
           sAlert.success("Thanks! Your comment has been posted!");
         } else {
           sAlert.error(error.reason);
+          return;
         }
       });
-
+      $('#replyText-' + this._id).val(" ");
+      $(".newcomment-" + this._id).hide();
+      Cookies.set("submitted" + this._id, true);
+      $(".commentParent-" + this._id).hide();
+      Session.set("showingComments" + this._id, "false")
     }
   },
   'keyup .replyText': function() {
