53f412121d9ca2f103510ad7771e75f7717cafb0
Ash Smith <ash@getsaven.com>
2018-01-19 12:40:59 +0800

    problem: No ability to filter currencies
    solution: Add basic filtering to filter currencies reactively. I have no added all attributes until we defined what attributes need filtering on. Also some attribues will rely on ui elelements. e.g. date/time lookups.

--- client/main.js
+++ client/main.js
@@ -4,3 +4,8 @@ import { Accounts } from 'meteor/accounts-base';
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
+
+//Global helper to easily get session values in templates
+Template.registerHelper( 'session', ( name ) => {
+  return Session.get(name);
+});
