22b3bc4d4ad619a4506d4c5296ac45c506410d0c
Ash Smith <missenlinx@gmail.com>
2018-03-21 09:37:05 +0800

    problem:images cannot be expanded
    solution: update helpers to exclude . from the jquery lookup. dots on lookup are not supprted #651

--- imports/ui/pages/problems/problemImages.js
+++ imports/ui/pages/problems/problemImages.js
@@ -9,6 +9,11 @@ Template.problemImages.helpers({
 })
 
 Template.problemImage.helpers({
+	_id: function() {
+        let img = this.split('.')
+
+        return img[0];
+    },
     thumbnail: function() {
         let img = this.split('.')
 
