ec35cdb28f8e73d6f4859224ddf685573217dc33
Andrej Budinčević <andrew@hotmail.rs>
2018-04-07 21:25:19 +0200

    Problem: No client side tests (#1001)
    Solution: Implement webdriverio + headless chrome client side tests in Travis CI and include an example test for the home page. (`homepage.spec.js`)

---
+++ wdio.conf.js
@@ -0,0 +1,27 @@
+exports.config = {
+    specs: ['./imports/**/*.ui-test.js'],
+    exclude: [],
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', '--disable-gpu', '--window-size=1280,800'],
+            binary: '/usr/bin/google-chrome-stable'
+        }
+    }],
+    sync: true,
+    logLevel: 'silent',
+    coloredLogs: true,
+    deprecationWarnings: true,
+    bail: 0,
+    screenshotPath: './screens/',
+    baseUrl: 'http://localhost:3000',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    framework: 'mocha',
+    reporters: ['dot'],
+    mochaOpts: {
+        ui: 'bdd'
+    }
+}
