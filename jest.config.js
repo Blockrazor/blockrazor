1971f08fcec2f714bef527cd8391c3ccea97c862
zhan <jey.and.key@gmail.com>
2018-04-11 22:34:21 +0600

    Problem: testing if anbud got jest going in devops
    Solution: push a jest test see if it fails.

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+
+// module.exports = {
+//   moduleFileExtensions: [
+//     'js',
+//   ],
+//   modulePaths: [
+//     '<rootDir>/node_modules/',
+//     '<rootDir>/node_modules/jest-meteor-stubs/lib/',
+//   ],
+//   moduleNameMapper: {
+//     "meteor/(.*)": "<rootDir>/tests/mocks/$1.js",
+//     '^(.*):(.*)$': '$1_$2',
+//   },
+//   unmockedModulePathPatterns: [
+//     '/^imports\\/.*\\.jsx?$/',
+//     '/^node_modules/',
+//   ],
+// };
