58dbb09eddcb454978800b52f4888bff133d16b1
Ash Smith <missenlinx@gmail.com>
2018-04-14 16:14:14 +0800

    problem: cant track user actions
    solution: Implement segmentIO to track certain user events

--- imports/api/users/users.js
+++ imports/api/users/users.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { UserData} from '/imports/api/indexDB.js';
 import { updateUsersStats } from './usersStats'
+import Analytics from 'analytics-node'
+var analytics = new Analytics('auAfRv1y0adOiVyz1TZB9nl18LI9UT98');
+
 
 //this needs looking into
 import { log } from '/server/main.js'
@@ -12,7 +15,21 @@ import { log } from '/server/main.js'
 
 //is in fact server only file
 
- 
+ /* function to init segment users, this is required 
+for us to map userId in meteor to transactions in 
+segment.io */
+
+function initSegmentIOUser(userId,email,fullname){
+  analytics.identify({
+  userId:userId,
+  traits: {
+    name: fullname,
+    email: email,
+    createdAt: new Date()
+  }
+});
+}
+
 Meteor.users.friendlySlugs({
   slugFrom: 'username',
   slugField: 'slug',
@@ -41,6 +58,10 @@ Accounts.validateLoginAttempt(function(result){
   });
   
   Accounts.onLogin(function(user){
+
+     //init segment.IO user
+    initSegmentIOUser(user.user._id,user.user.username,user.user.email)
+    
         //let's check if the user has logged in multiple users on the same ip address 
         Meteor.call('getUserConnectionInfo',
           (error, result) => {
