d97cb0cd7002e15f944983c4a431e3b130fa1c85
Andrej Budinčević <andrew@hotmail.rs>
2018-05-14 00:15:40 +0200

    Problem: Collections in global scope may only be used in development (#1527)
    Solution: Import all collections dynamically and proxy global collection calls on production to prevent their global usage.

--- imports/startup/client/testingCollectionGlobals.js
+++ imports/startup/client/testingCollectionGlobals.js
@@ -1,11 +1,15 @@
-if (Meteor.isDevelopment) {
-import *
-    as
-    collections
-    from
-    '/imports/api/indexDB.js'
+import * as collections from '/imports/api/indexDB.js'
 
-    for (let a in collections) {
-        window[a] = collections[a]
-    }
+for (let a in collections) {
+	// console.log(window[a])
+	if (!window[a]) { // don't override existing globals
+		if (Meteor.isDevelopment) {
+	    	window[a] = collections[a]
+		} else {
+			window[a] = {}
+			window[a]['find'] = window[a]['findOne'] = () => {
+				throw new Meteor.Error('Error.', `You can't reference ${a} globally when in production, please dynamically import it accordingly.`)
+			}
+		}
+	}
 }
