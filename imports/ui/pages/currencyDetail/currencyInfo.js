b8697fe09bca98d33c2aa8e9393f7ffb1a028d50
Chamidu Supeshala <chamidusupeshala@gmail.com>
2018-05-18 12:22:00 +0530

    problem: the icon of cancel button broken everywhere #1551
    solution: replaced old font awesome cancel icon name with new one

--- imports/ui/pages/currencyDetail/currencyInfo.js
+++ imports/ui/pages/currencyDetail/currencyInfo.js
@@ -42,7 +42,7 @@ Template.currencyInfo.onRendered(function () {
   const self = this
 
   $.fn.editable.defaults.mode = 'inline' // display them inline
-  $.fn.editableform.buttons = `<button type="submit" class="btn btn-primary btn-sm editable-submit"><i class="fa fa-check"></i></button><button type="button" class="btn btn-default btn-sm editable-cancel"><i class="fa fa-close"></i></button>` // custom buttons with fa icons
+  $.fn.editableform.buttons = `<button type="submit" class="btn btn-primary btn-sm editable-submit"><i class="fa fa-check"></i></button><button type="button" class="btn btn-default btn-sm editable-cancel"><i class="fa fa-times"></i></button>` // custom buttons with fa icons
 
   // editable fields
   let editables = ['currencyName', 'currencySymbol', 'gitRepo', 'officialSite', 'previousNames','smartContractURL']
