ee67be4ef758e4081963104879e753d60953f9e8
Andrej Budinčević <andrew@hotmail.rs>
2018-05-21 20:55:47 +0200

    Problem: Pending bounties and rejected bounties not shown (#1614)
    Solution: Fix the logic for displaying pending and rejected bounties on `/mypending` page. Include basic bounty data in the card.

---
+++ imports/ui/pages/userPendingCurrencies/userPendingBounty.js
@@ -0,0 +1,22 @@
+import { Template } from 'meteor/templating'
+
+import './userPendingBounty.html'
+
+Template.userPendingBounty.helpers({
+	title: () => {
+		let templ = Template.instance().data
+
+		if (templ.type && templ.type.includes('currency-')) { // currency-x
+			let curName = templ.type.replace('currency-', '')
+			curName = `${curName[0].toUpperCase()}${curName.substring(1)}`
+
+			return `Hash power API call for ${curName}`
+		}
+
+		let title = templ.type && templ.type.replace('new-', '') // new-codebase, new-wallet, new-community, new-currency
+		title = `${title[0].toUpperCase()}${title.substring(1)}`
+
+		return `${title} bounty`
+	},
+	fixed: val => val && parseFloat(val).toFixed(2)
+})
