b4b1b2b5d3ea2a5de6a76e8c86fd5677b68fddaf
Samuel Ralak <samuelralak@hotmail.com>
2018-05-26 17:17:31 +0300

    Problem: [/moderator/flagged] Bad UI/UX #1278
    Solution: Created dynamic route supporting different collection items. Displaying one flagged item at item and Bootswatchified the page

---
+++ imports/ui/pages/moderator/flagged/flaggedItem.js
@@ -0,0 +1,95 @@
+import { Template } from 'meteor/templating'
+import { Features, Redflags } from '/imports/api/indexDB.js'
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
+
+import './flaggedItem.html'
+
+const collections = { Features, Redflags }
+
+const getCollectionKlass = (collection) => {
+    return collection.replace(collection[0], collection[0].toUpperCase())
+}
+
+const nextFlaggedItem = (klass) => {
+    var nextCollections = _.omit(collections, klass)
+    var objectKeys = _.shuffle(Object.keys(nextCollections))
+	var nextFlagged = []
+
+		for (let index = 0; index < objectKeys.length; index++) {
+			const element = objectKeys[index];
+			nextFlagged = nextCollections[element].find({ 
+                flagRatio: { $gt: 0.6 },
+                "mod.votes" : { "$not" : { "$elemMatch" : { "userId" : Meteor.userId()  } } } 
+            }).fetch();
+
+			if (nextFlagged.length > 0) { 
+				const sample = _.sample(nextFlagged)
+				FlowRouter.go('/moderator/flagged/' + element.toLowerCase() + '/' + sample._id)
+				break 
+			}
+		}
+
+        FlowRouter.go('/moderator/flagged')
+		return
+}
+
+Template.flaggedItem.onCreated(function() {
+	this.autorun(() => {
+		SubsCache.subscribe('features')
+		SubsCache.subscribe('redflags')
+		SubsCache.subscribe('users')
+    })
+})
+
+Template.flaggedItem.helpers({
+	flaggedItem: () => {
+        const klass = getCollectionKlass(FlowRouter.getParam('collection'))
+        return collections[klass].findOne({ _id: FlowRouter.getParam('id') })
+    },
+	pardons: () => UserData.find({
+		'pardon.status': 'new'
+	}),
+	username: function() {
+		return (Meteor.users.findOne({
+			_id: this.createdBy
+		}) || {}).username
+	},
+	text: function() {
+		return this.featureName || this.comment || this.name // something has to be defined
+	},
+	voted: function() {
+		return !!((this.mod || {}).votes || []).filter(i => i.userId === Meteor.userId()).length
+	},
+	upvotes: function() {
+		return (this.mod || {}).upvotes || 0
+	},
+	downvotes: function() {
+		return (this.mod || {}).downvotes || 0
+	}
+})
+
+Template.flaggedItem.events({
+	'click .js-vote': function(event, templateInstance) {
+        let type = $(event.currentTarget).data('vote')
+
+        Meteor.call('flaggedVote', this._id, type, this.comment ? 'comment' : (this.featureName ? 'features' : 'redflags'), (err, data) => {
+            if (err && err.error === 'mod-only') {
+                sAlert.error('Only moderators can vote')
+            }
+
+            if (data === 'ok') {
+                sAlert.success('Deleted.')
+            } else if (data === 'not-ok') {
+            	sAlert.success('Flag removed.')
+            }
+        })
+
+        const klass = getCollectionKlass(FlowRouter.getParam('collection'))
+        nextFlaggedItem(klass)
+    },
+
+    'click #nextFlaggedItem': (event) => {
+        const klass = getCollectionKlass(FlowRouter.getParam('collection'))
+        nextFlaggedItem(klass)
+    }
+})
