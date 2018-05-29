85eb641c4430a86a1afba179be2d14db09d1a8b6
Samuel Ralak <samuelralak@hotmail.com>
2018-05-29 23:20:35 +0300

    Problem: exchanges can easily be ignored #1682
    Solution: displaying one proposed exchange at a time. improved page styling

---
+++ imports/ui/pages/moderator/exchanges/removeExchange.js
@@ -0,0 +1,78 @@
+import { Template } from 'meteor/templating'
+import { UserData, Exchanges } from '/imports/api/indexDB.js'
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
+
+import './removeExchange.html'
+
+var nextExchange = () => {
+    var sample = _.sample(Exchanges.find({
+        _id : { $ne : FlowRouter.getParam('id') },
+        removalProposed: true,
+        votes : { 
+            "$not" : { 
+                "$elemMatch" : { "userId" : Meteor.userId()  } 
+            } 
+        }
+    }).fetch())
+
+    if (sample === undefined) { 
+        FlowRouter.go('/moderator/exchanges') 
+    } else {
+        FlowRouter.go('/moderator/exchanges/' + sample._id)
+    }
+}
+
+Template.removeExchange.onCreated(function() {
+	this.autorun(() => {
+		SubsCache.subscribe('modExchanges')
+	})
+})
+
+Template.removeExchange.helpers({
+	exchanges: function() {
+		return Exchanges.find({
+			removalProposed: true
+		})
+    },
+    exchange: function() {
+        return Exchanges.findOne({ _id : FlowRouter.getParam('id') })
+    },
+	currencies: function() {
+		return this.currencies.map(i => i.name).toString().replace(/,/ig, ', ')
+	},
+	voted: function() {
+		return !!(this.votes || []).filter(i => i.userId === Meteor.userId()).length
+	},
+	upvotes: function() {
+		return this.upvotes || 0
+	},
+	downvotes: function() {
+		return this.downvotes || 0
+	},
+	score: function() {
+		return this.score || 0
+	}
+})
+
+Template.removeExchange.events({
+	'click .js-vote': function(event, templateInstance) {
+        let type = $(event.currentTarget).data('vote')
+
+        Meteor.call('exchangeVote', this._id, type, (err, data) => {
+            if (err && err.error === 'mod-only') {
+                sAlert.error('Only moderators can vote')
+            }
+
+            if (data === 'ok') {
+                sAlert.success('Successfully removed.')
+            } else if (data === 'not-ok') {
+            	sAlert.success('Removal denied.')
+            }
+
+            nextExchange()
+        })
+    },
+    'click #skipChange': () => {
+        nextExchange()
+    }
+})
