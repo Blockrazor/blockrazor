21c485c3ca1cba30d8ec6b2d6fa1542a77713ebe
Madan Bhandari <madan.kbh@gmail.com>
2018-09-11 15:19:58 +0530

    Problem : Landing page content (UI-UX) #1854
    Solution : 1. Layout changed for intro banner
    2. Signup form added in home banner
    3. More Features added

--- imports/ui/pages/landingpage/landingpage.js
+++ imports/ui/pages/landingpage/landingpage.js
@@ -1,13 +1,17 @@
 import './landingpage.html'
+import './landingpage.scss'
+import '../signup/signup'
 
 Template.landingpage.onRendered(function () {
   // hide the menu when the landing page is open
-  $('body').removeClass("sidebar-md-show")
-});
+  $('body').removeClass('sidebar-md-show')
+  // hide breadcrumb
+  $('.breadcrumb').hide()
+})
 
 Template.landingpage.helpers({
-  startUrl(){
-    if(Meteor.userId()){
+  startUrl () {
+    if (Meteor.userId()) {
       return '/home'
     } else {
       return '/login'
