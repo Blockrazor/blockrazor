c3941541179f84c58b329e2e16b66f2775651619
Samuel Ralak <samuelralak@hotmail.com>
2018-09-16 19:24:20 +0300

    problem: breadcrumbs do not show what currency you are looking at #1950
    solution: added param to breadcrumb option, appending param to breadcrumb text if it exists

--- imports/ui/layouts/layout.js
+++ imports/ui/layouts/layout.js
@@ -8,11 +8,21 @@ Template.layout.helpers({
         bc.urls = bc.urls || []
         bc.urls.push(FlowRouter.current().path)
 
-        return crumbs.map((i, ind) => ({
-        	text: TAPi18n.__(`breadcrumbs.${i.trim()}`),
-        	url: bc.urls[ind],
-        	notLast: ind !== crumbs.length - 1
-        }))
+        return crumbs.map((i, ind) => {
+            if (bc.param !== undefined && i.trim() === bc.param)
+                i = FlowRouter.getParam(bc.param)
+
+            let text = TAPi18n.__(`breadcrumbs.${i.trim()}`)
+            
+            if (text.split('.')[0] === 'breadcrumbs')
+                text = text.split('.')[text.split('.').length - 1]
+
+            return {
+                text: text,
+                url: bc.urls[ind],
+                notLast: ind !== crumbs.length - 1
+            }
+        })
     }
 })
 
