a3b98fd007df22350116369b06597796dd14ccaf
Madan Bhandari <madan.kbh@gmail.com>
2018-09-19 12:13:24 +0530

    Problem: forget password email not working
    Solution : Email URL issue fixed

--- server/main.js
+++ server/main.js
@@ -1,30 +1,21 @@
-import { Meteor } from 'meteor/meteor';
-import { AppLogs, UserData, Bounties} from '/imports/api/indexDB.js';
-import { generateBounties, createtypes, fetchHashrate } from '/imports/api/utilities'
-
 import '/imports/startup/server'
-
-//import * as jobs from './API_requests/github.js';
-
 
 SyncedCron.add({
   name: 'Tabulate ELO values',
-  schedule: function(parser) {
+  schedule: function (parser) {
     return parser.text('every 10 minutes');
   },
-  job: function() {
-    Meteor.call('tabulateElo', (err, data) => {})
+  job: function () {
+    Meteor.call('tabulateElo', (err, data) => { })
   }
-}); 
+})
 
 // FastRender.route('/', function(){
 // SubsCache.subscribe('approvedcurrencies');
 // });
 
 Meteor.startup(() => {
-//jobs.initiate_later();
-SyncedCron.start();
-Meteor.call('convertAlgorithm', (err, data) => {})
-//jobs.print();
-  // code to run on server at startup
-});
+  MAIL_URL = process.env.MAIL_URL
+  SyncedCron.start()
+  Meteor.call('convertAlgorithm', (err, data) => { })
+})
