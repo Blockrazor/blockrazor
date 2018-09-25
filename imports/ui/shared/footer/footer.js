3e7c269cc4b62089f41ff21266973ad78893cf1c
Madan Bhandari <madan.kbh@gmail.com>
2018-09-25 22:40:46 +0530

    Problem: difficult to change language #1993
    Solution : Language change options added on footer

--- imports/ui/shared/footer/footer.js
+++ imports/ui/shared/footer/footer.js
@@ -1,1 +1,27 @@
 import './footer.html'
+import { Cookies } from 'meteor/ostrio:cookies'
+const cookies = new Cookies()
+
+Template.footer.onCreated(function () {
+  this.defaultLanguage = 'en'
+  TAPi18n.setLanguage(cookies.get('language') || 'en')
+})
+
+Template.footer.helpers({
+  languages: () => {
+    return Object.keys(TAPi18n.languages_names).map(key => {
+      return {
+        code: key,
+        name: TAPi18n.languages_names[key][1],
+        selected: key === TAPi18n.getLanguage()
+      }
+    })
+  }
+})
+
+Template.footer.events({
+  'change #selectLanguage' (event) {
+    event.preventDefault()
+    TAPi18n.setLanguage(event.target.value)
+  }
+})
