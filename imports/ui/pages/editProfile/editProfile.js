20bf937a18acc46d3e068e773884e12e4009a2a4
Andrej Budinčević <andrew@hotmail.rs>
2018-10-05 14:08:48 +0200

    Problem: Uploading a new profile picture is annoying (#2039)
    Solution: Add an upload image button under the profile picture and remove it from edit profile tab.

--- imports/ui/pages/editProfile/editProfile.js
+++ imports/ui/pages/editProfile/editProfile.js
@@ -86,69 +86,6 @@ Template.editProfile.events({
             templateInstance.disable2fa.set(false)
             templateInstance.enable2fa.set(false)
         }
-    },
-
-    'click #image': (event, templateInstance) => {
-        event.preventDefault()
-
-        $('#imageInput').click()
-    },
-    'change #imageInput': (event, templateInstance) => {
-        let file = event.target.files[0]
-        let uploadError = false
-        let mimetype = mime.lookup(file)
-        let fileExtension = mime.extension(file.type)
-
-        if (file.size > _profilePictureFileSizeLimit) {
-            sAlert.error(TAPi18n.__('user.edit.too_big'))
-            uploadError = true
-        }
-
-        if (!_supportedFileTypes.includes(file.type)) {
-            sAlert.error(TAPi18n.__('user.edit.must_be_image'))
-            uploadError = true
-        }
-
-        if(file){
-        $('#uploadLabel').removeClass('btn-success');
-        $('#uploadLabel').addClass('btn-primary');
-        $("button").attr("disabled", "disabled"); //disable all buttons
-        $(".uploadText").html(`<i class='fa fa-circle-o-notch fa-spin'></i> ${TAPi18n.__('user.edit.uploading')}`); //show upload progress
-
-
-        //Only upload if above validation are true
-        if (!uploadError) {
-            let reader = new FileReader()
-            reader.onload = fEvent => {
-                let binary = reader.result
-                let md5 = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(binary)).toString()
-                
-                Meteor.call('uploadProfilePicture', file.name, reader.result, md5, (error, result) => {
-                                        if (error) {
-                        sAlert.error(error.message);
-                        $('#uploadLabel').removeClass('btn-success');
-                        $('#uploadLabel').addClass('btn-primary');
-                        $(".uploadText").html(TAPi18n.__('user.edit.upload'));
-                    } else {                        
-                    $('#js-image').val(`${md5}.${fileExtension}`)
-                    $("button").attr("disabled", false); //enable all buttons
-                    $('#uploadLabel').addClass('btn-success');
-                    $(".uploadText").html(TAPi18n.__('user.edit.change')); //update button text now upload is complete
-                    $('#profilePicture').attr('src', `${_profilePictureUploadDirectoryPublic}${md5}_thumbnail.${fileExtension}`)
-
-                    //send an event to segment
-        let payload = {
-            event: 'Changed their profile picture',
-        }
-
-        segmentEvent(payload);
-
-                    }
-                })
-           }
-           reader.readAsBinaryString(file)
-        }
-    }
     }
 })
 
